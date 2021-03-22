78cd5082bb7547de4200f68d13430dd8a683fa14
Madeleine Barr <madeleine.barr13@gmail.com>
2021-03-22 12:01:59 -0400

    finished all dropdowns

--- testproject/client/src/components/FilterByPlace.js
+++ testproject/client/src/components/FilterByPlace.js
@@ -24,10 +24,33 @@ class FilterByPlace extends React.Component {
         return (
             <select artist_nationality={this.props.artist_nationality} culture={this.props.culture} country={this.props.country} onChange={this.handlePlaceFilterChange}>
             
-            <option artist_nationality="" culture="" country="">Select a region</option>
+            <option artist_nationality="" culture="" country="">Select a nationality</option>
             <option artist_nationality="" culture="" country="">All</option>
+            <option artist_nationality="American" culture="American" country="United States">American</option>
+            <option artist_nationality="Egyptian" culture="Egyptian" country="Egypt">Egyptian</option>
+            <option artist_nationality="French" culture="French" country="France">French</option>
+            <option artist_nationality="Persian" culture="Persian" country="Iran">Persian</option>
+            <option artist_nationality="Afghani" culture="Afghani" country="present-day Afghanistan">Afghani</option>
+            <option artist_nationality="Indian" culture="India" country="India">Indian</option>
+            <option artist_nationality="Chinese" culture="China" country="China">Chinese</option>
+            <option artist_nationality="Japanese" culture="Japan" country="Japan">Japanese</option>
+            <option artist_nationality="Peruvian" culture="Peru" country="Peru">Peru</option>
+            <option artist_nationality="Turkish" culture="Turkish" country="Turkey">Turkish</option>
+            <option artist_nationality="Greek, Attic" culture="Greek" country="Greece">Greek</option>
+            <option artist_nationality="Italian" culture="Italian" country="Italy">Italian</option>
+            <option artist_nationality="Austrian" culture="Austrian" country="Austria">Austria</option>
+            <option artist_nationality="Mexican" culture="Mexican" country="Mexico">Mexican</option>
+            <option artist_nationality="Dutch" culture="Dutch" country="Belgium">Dutch</option>
+            <option artist_nationality="Netherlandish" culture="South Netherlandish" country="The Netherlands">Netherlandish</option>
             <option artist_nationality="German" culture="German" country="Germany">German</option>
-            <option artist_nationality="Egyptian" culture="Egyptian" country="Egypt">Egypt</option>
+            <option artist_nationality="British" culture="British" country="England">British</option>
+            <option artist_nationality="Spanish" culture="Spanish" country="Spain">Spanish</option>
+            <option artist_nationality="Mayan" culture="Maya" country="Maya">Mayan</option>
+            <option artist_nationality="Byzantine" culture="Byzantine" country="Byzantine">Byzantine</option>
+            <option artist_nationality="Korean" culture="Korea" country="Korea">Korean</option>
+            <option artist_nationality="Assyrian" culture="Assyrian" country="Assyria">Assyrian</option>
+            <option artist_nationality="Tibetan" culture="Tibetan" country="Tibet">Tibetan</option>
+            
         </select>
         )
     }
--- testproject/client/src/components/FilterByTheme.js
+++ testproject/client/src/components/FilterByTheme.js
@@ -16,9 +16,32 @@ class FilterByTheme extends React.Component {
             <select theme={this.props.theme} onChange={this.handleThemeFilterChange}>
                 <option theme="">Select a theme</option>
                 <option theme="">All</option>
+                <option theme="Self-portraits">Self-portraits</option>
                 <option theme="Moon">Moon</option>
                 <option theme="Lakes">Lakes</option>
+                <option theme="Boats">Boats</option>
+                <option theme="Trees">Trees</option>
+                <option theme="Flowers">Flowers</option>
+                <option theme="Mountains">Mountains</option>
+                <option theme="Seascapes">Seascapes</option>
+                <option theme="Landscapes">Landscapes</option>
+                <option theme="Madonna and Child">Madonna and Child</option>
+                <option theme="Virgin Mary">Virgin Mary</option>
+                <option theme="Jesus">Jesus</option>
+                <option theme="Venus">Venus</option>
+                <option theme="Women">Women</option>
+                <option theme="Portraits">Portraits</option>
+                <option theme="Sun">Sun</option>
+                <option theme="Hieroglyphs">Hieroglyphs</option>
+                <option theme="Arabic">Arabic</option>
+                <option theme="Birds">Birds</option>
+                <option theme="Food">Food</option>
+                <option theme="Mythical Creatures">Mythical Creatures</option>
+                <option theme="Buddhism">Buddhism</option>
+                <option theme="Tombs">Tombs</option>
+                <option theme="Battles">Battles</option>
             </select>
+
         )
     }
 }
