f187bb3597da5b3a7f59a286629197dbf8f1337f
Madeleine Barr <madeleine.barr13@gmail.com>
2021-03-25 14:49:11 -0400

    styled nav bar

--- testproject/client/src/components/NavBar.js
+++ testproject/client/src/components/NavBar.js
@@ -2,8 +2,9 @@ import React from 'react';
 // we don't have prop types yet but we will import it
 
 const NavBar = () => (
-    <nav style={{backgroundColor: 'grey', height: '50px', marginTop: '-12px', color: 'white'}}>
-        <p style={{marginLeft: '15px'}}>The Met</p>
+    <nav className="navBar">
+        <img className="navBarMetLogo" src="../metlogo.png" alt="met logo"></img>
+
     </nav>
 );
 
